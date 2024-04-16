b29cc29b16c0c3446d899515c5e3ed86e98ea5c0
Wilinton Gabriel <wilintongabrielss1@gmail.com>
2024-04-15 21:53:59 -0300

    importando as referencias para manipular o DOM

--- src/script.js
+++ src/script.js
@@ -1,1 +1,10 @@
-console.log("olá mundo");
+const menu = document.getElementById("main");
+const modal = document.getElementById("cart-modal");
+const cartBtn = document.getElementById("cart-btn");
+const cartItemsContainer = document.getElementById("cart-items");
+const cartTotal = document.getElementById("cart-total");
+const checkoutBtn = document.getElementById("checkout-btn");
+const closeModalBtn = document.getElementById("close-modal-btn");
+const cartCounter = document.getElementById("cart-count");
+const addressInput = document.getElementById("address");
+const addressWarn = document.getElementById("address-warn");
